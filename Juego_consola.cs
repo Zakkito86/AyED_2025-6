31c8967323a7a3153344a8845b0b76f1d8ea8c9a
Telavonai <matiianovale@gmail.com>
2026-03-26 17:01:17 -0300

    Add files via upload

---
+++ Juego_consola.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string game;
+            string console;
+            Console.WriteLine("hola usuario");
+            Console.Write("¿cual es tu nombre?: ");
+            name = Console.ReadLine();
+            Console.Write("¿cual es tu Juego favorito?: ");
+            game = Console.ReadLine();
+            Console.Write("en que consola Jugas?: ");
+            console = Console.ReadLine();
+            Console.WriteLine("hola " + name + " tu juego favorito es " + game + " y lo jugas en " + console);
+
+        }
+    }
+}
