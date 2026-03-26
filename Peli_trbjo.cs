b7371dfb62b67a4167f41d83613e5487d8826b99
Telavonai <matiianovale@gmail.com>
2026-03-26 16:21:21 -0300

    peli_trbj

---
+++ Peli_trbjo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string movie;
+            Console.WriteLine("hola usuario");
+            Console.Write("¿cual es tu nombre?: ");
+            name = Console.ReadLine();
+            Console.Write("¿cual es tu pelicula favorita?: ");
+            movie = Console.ReadLine();
+
+            Console.WriteLine("hola querido usuario " + name +" "+ movie + " es una porqueria");
+
+            
+        }
+    }
+}
