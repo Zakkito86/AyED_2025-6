55ba80160bab2c20e8f0455b65e43c83319979ef
Telavonai <matiianovale@gmail.com>
2026-04-09 16:37:13 -0300

    starwars

---
+++ opciones SW.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cristalKyber;
+
+            Console.WriteLine("1. Lo devuelves al templo.", "hola");
+            Console.WriteLine("2. Lo vendes por créditos.");
+            Console.WriteLine("3. Lo usas para construir un arma roja");
+            Console.WriteLine("ingrese un valor entre 1, 2 y 3 ");
+            cristalKyber = Convert.ToInt32(Console.ReadLine());
+
+            if (cristalKyber == 1)
+            {
+                Console.WriteLine("1. el camino jedi sigue");
+            }
+            else if (cristalKyber == 2)
+            {
+                Console.WriteLine("2. un cazarrecompensas eres");
+                    }
+            else if (cristalKyber == 3)
+            {
+                Console.WriteLine("3. el lado oscuro se apodera de ti");
+            }
+        }
+    }
+}
---
+++ precio C3POS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int preciorobots = 2000;
+            int robots;
+
+            Console.WriteLine("ingrese la cantidad de robots a comprar: ");
+            robots = Convert.ToInt32(Console.ReadLine());
+
+            double precio = robots * preciorobots;
+            if (robots >= 3)
+            {
+                double preciototal = precio * 0.2;
+                double preciofinal = precio - preciototal;
+                Console.WriteLine("el precio final con descuento es " + preciofinal + " sin descuento es " + precio);
+            }
+            else
+                Console.WriteLine("el precio final no tiene descuento y es " + precio );
+
+
+        }
+    }
+}
