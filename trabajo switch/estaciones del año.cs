29ef8d1b5b06f4e46dd1d5dd792e7c01d5395203
Telavonai <matiianovale@gmail.com>
2026-05-08 14:39:05 -0300

    Add files via upload

---
+++ trabajo switch/estaciones del año.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("elija su estacion favorita del año");
+            Console.WriteLine("1. primavera");
+            Console.WriteLine("2. verano");
+            Console.WriteLine("3. otoño");
+            Console.WriteLine("4. invierno");
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine(" has elegido la primavera ");
+                    break;
+
+                case 2:
+                    Console.WriteLine(" has elegido el verano ");
+                    break;
+
+                case 3:
+                    Console.WriteLine(" has elegido el otoño ");
+                    break;
+
+                case 4:
+                    Console.WriteLine(" has elegido el invierno ");
+                    break;
+
+                default:
+                    Console.WriteLine(" elija una opcion valida por favor ");
+                    break;
+            }
+        }
+    }
+}
