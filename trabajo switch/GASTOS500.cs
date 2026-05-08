957e0b4b7e08c0c4a421f3781300752f17d7acc6
Telavonai <matiianovale@gmail.com>
2026-05-08 16:56:31 -0300

    Add files via upload

---
+++ trabajo switch/GASTOS500.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int precio = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                int numero = 0;
+
+                Console.Write("ingrese el gasto ");
+                numero = int.Parse(Console.ReadLine());
+                precio = precio + numero;
+
+                if (numero > 500)
+                {
+                    contador++;
+                }
+                Console.WriteLine("hizo " + contador + " veces gastos mayores a 500 en el dia ");
+                Console.WriteLine("el gasto total fue de " + precio);
+            }
+        }
+    }
+}
---
+++ trabajo switch/TEMPERATURA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int temperatura = 0;
+            for (int i = 1; i <= 7; i++)
+            {
+                int numero = 0;
+
+                Console.Write("ingrese la temperatura de ese dia ");
+                numero = int.Parse(Console.ReadLine());
+                temperatura = temperatura + numero;
+
+                if (numero < 0)
+                {
+                    contador++;
+                }
+                Console.WriteLine("la temperatura que hizo en la semana fue menor a 0, " + contador + " veces" );
+                Console.WriteLine("la temperatura total es de " + temperatura);
+            }
+        }
+    }
+}
