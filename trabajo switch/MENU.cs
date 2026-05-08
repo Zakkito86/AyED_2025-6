e195a998feceaac896ae9c57f18686e02a641fc7
Telavonai <matiianovale@gmail.com>
2026-05-08 15:47:30 -0300

    Add files via upload

---
+++ trabajo switch/MENU.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("elija la comida que desea pedir para conocer su precio");
+            Console.WriteLine("(H)amburguesa ");
+            Console.WriteLine("(P)izza ");
+            Console.WriteLine("(S)andwich ");
+
+            string opcion = Console.ReadLine();
+
+
+            switch (opcion)
+            {
+                case "H":
+                    Console.WriteLine(" La hamburguesa cuesta 3500 ");
+                    break;
+
+                case "P":
+                    Console.WriteLine("La pizza cuesta 5000 ");
+                    break;
+
+                case "S":
+                    Console.WriteLine("El sandwich cuesta 2500 ");
+                    break;
+                
+                default:
+                    Console.WriteLine(" producto no encontrado ");
+                    break;
+            }
+        }
+    }
+}
---
+++ trabajo switch/NOTA.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elija su calificacion ");
+            Console.WriteLine(" A ");
+            Console.WriteLine(" B ");
+            Console.WriteLine(" C ");
+            Console.WriteLine(" D ");
+            Console.WriteLine(" F ");
+
+
+            String opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "A":
+                    Console.WriteLine(" has sacado un exelente ");
+                    break;
+
+                case "B":
+                    Console.WriteLine(" has sacado una buena nota ");
+                    break;
+
+                case "C":
+                    Console.WriteLine(" has sacado un regular ");
+                    break;
+
+                case "D":
+                    Console.WriteLine(" has sacado un suficiente ");
+                    break;
+
+                case "F":
+                    Console.WriteLine(" has sacado un reprobado ");
+                    break;
+
+                default:
+                    Console.WriteLine(" elija una opcion valida por favor ");
+                    break;
+            }
+        }
+    }
+}
