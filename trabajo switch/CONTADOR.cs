3040fa8a2dba5f4669090c872b09bc09eb4ce783
Telavonai <matiianovale@gmail.com>
2026-05-08 16:18:13 -0300

    Add files via upload

---
+++ trabajo switch/CONTADOR.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcancia = 0;
+
+            for (int i = 1 ; i <= 4 ; i++)
+            {
+
+                Console.WriteLine("ingrese su ingreso de la semana " + i);
+                int ingreso = int.Parse(Console.ReadLine());
+
+                alcancia = alcancia + ingreso;
+                Console.WriteLine("el ahorro total es de " + alcancia + " " + ingreso);
+            }
+        }
+    }
+}
