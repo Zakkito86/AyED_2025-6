a63569cb7d2188eff2c2b06de7b9a04e1bb02126
Telavonai <matiianovale@gmail.com>
2026-05-08 16:38:06 -0300

    Add files via upload

---
+++ trabajo switch/IDIOMA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elija el idioma para saludar ");
+            Console.WriteLine("1. INGLES ");
+            Console.WriteLine("2. FRANCES ");
+            Console.WriteLine("3 ALEMAN ");
+
+            string opcion = Console.ReadLine();
+
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine(" HELLO ");
+                    break;
+
+                case "2":
+                    Console.WriteLine(" SALUT ");
+                    break;
+
+                case "3":
+                    Console.WriteLine(" HALLO ");
+                    break;
+            }
+        }
+    }
+}
